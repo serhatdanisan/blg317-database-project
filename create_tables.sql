33bdc45fe873b51f289f6890133d8413f9314b61
serhatdanisan <119052876+serhatdanisan@users.noreply.github.com>
2024-11-08 02:49:57 +0300

    Create create_tables.sql
    MySQL file for creating tables

---
+++ create_tables.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE db
+USE db
+
+CREATE TABLE club (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    officialname VARCHAR(150) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    venue_id INT NOT NULL,
+    FOREIGN KEY (venue_id) REFERENCES football_stadiums(id)
+);
+
+CREATE TABLE match(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    dateutc DATETIME NOT NULL,
+    competition VARCHAR(100) NOT NULL,
+    venue_id INT NOT NULL,
+    home_club VARCHAR(20) NOT NULL,
+    away_club VARCHAR(20) NOT NULL,
+    winner VARCHAR(20) NOT NULL,
+    goal_by_home_club INT NOT NULL,
+    goal_by_away_club INT NOT NULL
+    FOREIGN KEY (venue_id) REFERENCES football_stadiums(id)
+);
+
